44d2445225400dc41de62611e1605fc3023386cb
Muhammed Nihal <nihal514t@gmail.com>
2026-05-22 15:06:03 +0530

    filter option added for categories

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,12 +5,15 @@ import ExpenseForm from "../components/ExpenseForm";
 import ExpenseList from "../components/ExpenseList";
 
 function Dashboard() {
+  const [filterCategory, setFilterCategory] = useState("");
+
   const [expenses, setExpenses] = useState(() => {
     const savedExpenses = localStorage.getItem("expenses");
-
     return savedExpenses ? JSON.parse(savedExpenses) : [];
   });
-
+  const filteredExpenses = filterCategory
+    ? expenses.filter((expense) => expense.category === filterCategory)
+    : expenses;
   useEffect(() => {
     localStorage.setItem("expenses", JSON.stringify(expenses));
   }, [expenses]);
@@ -32,8 +35,24 @@ function Dashboard() {
       <h1 className="text-4xl font-bold mb-6">Dashboard</h1>
 
       <ExpenseForm addExpense={addExpense} />
+      <div className="mb-6">
+        <select
+          value={filterCategory}
+          onChange={(e) => setFilterCategory(e.target.value)}
+          className="border p-3 rounded-lg bg-white"
+        >
+          <option value="">All Categories</option>
 
-      <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
+          <option value="Food">Food</option>
+
+          <option value="Travel">Travel</option>
+
+          <option value="Shopping">Shopping</option>
+
+          <option value="Bills">Bills</option>
+        </select>
+      </div>
+      <ExpenseList expenses={filteredExpenses} deleteExpense={deleteExpense} />
     </DashboardLayout>
   );
 }
